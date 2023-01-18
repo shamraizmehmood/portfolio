e3968cc114794c189712c02f559d97087d564fc8
Shamraiz Mehmood <shamraizmehmood3386@gmail.comm>
2023-01-18 12:02:56 +0000

    inital commit

---
+++ javascript/converter.js
@@ -0,0 +1,98 @@
+const api_url = 'http://www.floatrates.com/daily/usd.json'; // link the json data from api
+let total_amount = rate = totals =value= value_second= 0;
+let Source_currency =Destination_Currency=Current_Exchange_Rate=Calculation_Timestamp =Amount_of_transaction ="";
+let source_c= destination_c = currency_code =val_first=val_second= "";
+ 
+
+
+//------------------------------------------------------------------
+async function getData(){
+
+    const response = await fetch(api_url); // fetch the data from api
+    const jsonData = await response.json(); // strore json data in variabel
+    //------------------------------------------------------
+    var select=document.getElementById('dropdown')
+    var select_from =document.getElementById('dropdown-from')
+    for(let i in jsonData){
+       
+        var option = document.createElement('option');
+       var option_from = document.createElement('option');
+        var dropdown_value = document.getElementById("dropdown_value");
+
+       
+       option.text = option.value = jsonData[i].name +"    " +jsonData[i].code;
+       option_from.text = option_from.value = jsonData[i].name +"  " +jsonData[i].code;
+       select.add(option, 0);              
+       select_from.add(option_from, 0); 
+    }
+    //---------------------upper part value change--------------------------------------------
+     val_first = document.getElementById("dropdown").value.slice(-3);
+     value = jsonData[val_first.toLowerCase()]['inverseRate'];
+     Source_currency = jsonData[val_first.toLowerCase()]['name'];
+     Calculation_Timestamp = Date(Date.now());
+     var convert_first= document.getElementById("dropdown_value");
+     convert_first.innerHTML = value;
+    // ---------------------------lowwer part value change-----------------------------------
+    val_second = document.getElementById("dropdown-from").value.slice(-3);
+    value_second = jsonData[val_second.toLowerCase()]['rate'];
+    Destination_Currency = jsonData[val_second.toLowerCase()]['name'];
+    var convert_second= document.getElementById("dropdown_value_second");
+    convert_second.innerHTML = value_second;
+//--------------------------------------------------------------------------------------------
+    rate = value;
+    }
+
+function updateValue(){
+   
+    var val_first = document.getElementById("dropdown").value.slice(-3);
+    var val_second = document.getElementById("dropdown-from").value.slice(-3);
+
+     var value = jsonData[val_first.toLowerCase()]['rate'];
+
+     var convert_first= document.getElementById("dropdown_value");
+     convert_first.value = value;
+
+}
+
+ function opctfnt(){
+    document.getElementById("converter-outer").style.opacity='1';
+    document.getElementById("output_display").style.opacity = "1";
+    var total_amount=document.getElementById("Amount");
+    var input_amount = total_amount.value;
+    var total = value*input_amount*value_second;
+    var changer = value * value_second;
+
+    let Source_currency_value =document.getElementById('to_value');
+    Source_currency_value.innerHTML = Source_currency;
+
+
+    let Destination_Currency_value =document.getElementById('from_value');
+    Destination_Currency_value.innerHTML = Destination_Currency;
+
+    let Calculation_Timestamp_in  =document.getElementById('Timestamp');
+    Calculation_Timestamp_in.innerHTML = Calculation_Timestamp ;
+    console.log(Calculation_Timestamp.toString());
+
+    let Current_Exchange_Rate  =document.getElementById('display');
+    Current_Exchange_Rate.innerHTML = "1 "+ Source_currency+ " = " +changer + " "+ Destination_Currency ;
+
+    let Amount_of_transaction =document.getElementById('total');
+    Amount_of_transaction.innerHTML = input_amount +"  "+ val_first+" = "+ Math.floor(total) +" "+val_second ;
+
+    
+ 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/predictor.js
@@ -0,0 +1,233 @@
+function historicData() {
+    fetch("forcaster.json")
+        .then(res => res.json())
+        .then(data => {
+            let ArrayOfMonth = [];
+            let ArrayOfyear = [];
+            let gas = [];
+            let Electricity = [];
+            let Liquid_fuels = [];
+            let Solid_fuels = [];
+            var select = document.getElementById('dropdown-year')
+            for (let i in data) {
+                if (!ArrayOfyear.includes(data[i]["Year"])) {
+                    ArrayOfyear.push(data[i]["Year"])
+                }
+            }
+            for (let i in ArrayOfyear) {
+                var option = document.createElement('option');
+                option.text = option.value = ArrayOfyear[i];
+                select.add(option, 0);
+            }
+            for (let i = 0; i < 12; i++) {
+                gas.push(parseFloat(data[i]["Gas"].toFixed(1)));
+                Electricity.push(parseFloat(data[i]["Electricity"].toFixed(1)));
+                Liquid_fuels.push(parseFloat(data[i]["Liquid_fuels"].toFixed(1)));
+                Solid_fuels.push(parseFloat(data[i]["Solid_fuels"].toFixed(1)));
+
+            }
+            const ctx = document.getElementById('myChart');
+
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'Nov', 'Dec'],
+                    datasets: [{
+                        label: 'Electricity',
+                        data: Electricity,
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Gas',
+                        data: gas,
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Solid_fuels',
+                        data: Solid_fuels,
+                        borderWidth: 1
+                    },
+                    {
+                        label: Liquid_fuels,
+                        data: Liquid_fuels,
+                        borderWidth: 1
+                    }
+                    ],
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: false
+                        }
+                    }
+                }
+            });
+        })
+}
+
+function updateData() {
+    document.getElementById('graphContainer').style.display = '';
+    document.getElementById('graphContainer2').style.display = 'none';
+
+    fetch("forcaster.json")
+        .then(res => res.json())
+        .then(data => {
+
+            let ArrayOfMonth = [];
+            let ArrayOfyear = [];
+            let gas = [];
+            let Electricity = [];
+            let Liquid_fuels = [];
+            let Solid_fuels = [];
+            var selectYear = document.getElementById("dropdown-year");
+            let valued = selectYear.value;
+            for (let i in data) {
+                if (data[i]['Year'] == valued) {
+                    gas.push(parseFloat(data[i]["Gas"].toFixed(1)));
+                    Electricity.push(parseFloat(data[i]["Electricity"].toFixed(1)));
+                    Liquid_fuels.push(parseFloat(data[i]["Liquid_fuels"].toFixed(1)));
+                    Solid_fuels.push(parseFloat(data[i]["Solid_fuels"].toFixed(1)));
+                }
+            }
+
+            let chartExist = Chart.getChart('myChart')
+            chartExist.destroy();
+
+            const ctx = document.getElementById('myChart');
+
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'Nov', 'Dec'],
+                    datasets: [{
+                        label: 'Electricity',
+                        data: Electricity,
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Gas',
+                        data: gas,
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Solid_fuels',
+                        data: Solid_fuels,
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Liquid_fuels',
+                        data: Liquid_fuels,
+                        borderWidth: 1
+                    }
+                    ],
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: false
+                        }
+                    }
+                }
+            });
+        })
+}
+
+function manipulation(max, min, arr) {
+    let final = [];
+    let percentage = Math.round((max - min) / max) * 100 / 100;
+    let percent = (percentage * max);
+    for (let a in arr) {
+        addion = (parseFloat(percent) + parseFloat(arr[a])).toFixed(1);
+        final.push(parseFloat(addition));
+    }
+    return final
+}
+
+function prediction() {
+    document.getElementById('graphContainer2').style.display = '';
+    document.getElementById('graphContainer').style.display = 'none';
+
+    fetch("forcaster.json")
+        .then(res => res.json())
+        .then(data => {
+            let temp = 0
+            let ArrayOfMonth = [];
+            let ArrayOfyear = [];
+            let gas = [];
+            let Electricity = [];
+            let Liquid_fuels = [];
+            let Solid_fuels = [];
+            var selectYear = document.getElementById("dropdown-year");
+            let valued = selectYear.value;
+            for (let i in data.reverse()) {
+                if (data[i]['Year'] == valued) {
+                    gas.push(parseFloat(data[i]["Gas"].toFixed(1)));
+                    Electricity.push(parseFloat(data[i]["Electricity"].toFixed(1)));
+                    Liquid_fuels.push(parseFloat(data[i]["Liquid_fuels"].toFixed(1)));
+                    Solid_fuels.push(parseFloat(data[i]["Solid_fuels"].toFixed(1)));
+                    ArrayOfMonth.push(i["Month"]);
+                    temp++
+                    if (temp > 11) break;
+                }
+            }
+            let colculated = [gas, Solid_fuels, Liquid_fuels, Electricity];
+            for (let i = 0; i < 4; i++) {
+                colculated.splice(i, 1, manipulation(Math.max(...colculated[i], Math.min(...colculated[i]), colculated[i])));
+            }
+
+            ArrayOfMonth = ArrayOfMonth.reverse()
+            let chartExist = Chart.getChart('myChart2')
+            if (chartExist) {
+            chartExist.destroy();
+            }
+
+            const ctx2 = document.getElementById('myChart2');
+
+            new Chart(ctx2, {
+                type: 'line',
+                data: {
+                    labels: ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'Nov', 'Dec'],
+                    datasets: [{
+                        label: 'Electricity',
+                        data: Electricity,
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Gas',
+                        data: gas,
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Solid_fuels',
+                        data: Solid_fuels,
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Liquid_fuels',
+                        data: Liquid_fuels,
+                        borderWidth: 1
+                    }
+                    ],
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: false
+                        }
+                    }
+                }
+            });
+        });
+}
+
+// function manipulation(max, min, arr) {
+//     let final = [];
+//     let percentage = Math.round((max - min) / max) * 100 / 100;
+//     let percent = (percentage * max);
+//     for (let a in arr) {
+//         addion = (parseFloat(percent) + parseFloat(arr[a])).toFixed(1);
+//         final.push(parseFloat(addition));
+//         console.lo
+//     }
+//     return final
+// }
